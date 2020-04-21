506d55a67711058caae9cd8d67856b7825b5138f
Rodrigo <rodrigolemosrl@hotmail.com>
2020-04-20 21:43:05 -0300

    Appointments register, date validation and first model

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+const appointmentsRouter = Router();
+
+const appointments: Appointment[] = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentIsSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentIsSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = new Appointment(provider, parsedDate);
+
+  appointments.push(appointment);
+
+  return response.json(appointments);
+});
+
+export default appointmentsRouter;
