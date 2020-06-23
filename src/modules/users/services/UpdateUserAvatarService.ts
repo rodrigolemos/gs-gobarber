c62d8705de7aadd6a81acd4004af039c6f140797
Rodrigo <rodrigolemosrl@hotmail.com>
2020-06-23 18:44:08 -0300

    Configuring imports

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -2,9 +2,8 @@ import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 
-import AppError from '../../../shared/errors/AppError';
-
-import uploadConfig from '../../../config/upload';
+import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 import User from '../infra/typeorm/entities/User';
 
 interface Request {
